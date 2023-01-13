78890fb5d2b3bc7ad170aed9b99375b57e7ebe85
openkufo <openkufo@gmail.com>
2023-01-13 17:56:24 +0900

    refactor: reduce code length

--- src/com/paulhan/programmers/s120831/Solution.java
+++ src/com/paulhan/programmers/s120831/Solution.java
@@ -2,15 +2,11 @@ package com.paulhan.programmers.s120831;
 
 public class Solution {
 	public static void main(String[] args) {
-		new Solution().solution(10);
+		new Solution().solution(55);
 	}
 	
 	public int solution(int n) {
-        int answer = 0;
-        
-        for(int i = 1; i <= n; i++) {
-        	answer += i % 2 == 0 ? i : 0; 
-        }
+        int answer = (n/2) * (n/2) + n/2;
         return answer;
     }
 }
