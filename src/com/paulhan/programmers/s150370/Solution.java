9b70d760f76f0dc848563da3558f8ab949783e9a
paul Han <openkufo@gmail.com>
2023-01-17 22:47:42 +0900

    add Solution s150370

--- src/com/paulhan/programmers/s150370/Solution.java
+++ src/com/paulhan/programmers/s150370/Solution.java
@@ -1,9 +1,20 @@
 package com.paulhan.programmers.s150370;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
+
+    private final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+    private LocalDate expiredDate;
+    private Map<String, Integer> expiredDateMap = new HashMap<String, Integer>();
+
     public static void main(String[] args) throws ParseException {
         String date = "2022.05.19";
         String[] terms = {"A 6", "B 12", "C 3"};
@@ -18,6 +29,19 @@ public class Solution {
 
     public int[] solution(String today, String[] terms, String[] privacies) {
         int[] answer = {};
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
         return answer;
+    }
+
+    private initExp
+
+    private LocalDate getExpiredDate(String date){
+        return this.expiredDate;
+    }
+
+    private void setExpiredDate(String date, String term){
+        this.expiredDate = LocalDate.parse(date, DATE_TIME_FORMAT);
     }
 }
