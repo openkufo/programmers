db5f5270b91e5ce9eb1a355157cb96b942b381e6
paul Han <openkufo@gmail.com>
2023-02-02 14:41:18 +0900

    Add Solution 120908

---
+++ src/com/paulhan/programmers/s120908/Solution.java
@@ -0,0 +1,12 @@
+package com.paulhan.programmers.s120908;
+
+public class Solution {
+    public static void main(String[] args) {
+        int answer = new Solution().solution("ab6CDE443fgh22iJKlmn1o", "6CD");
+    }
+
+    public int solution(String str1, String str2) {
+        int answer = str1.contains(str2) ? 1 : 2;
+        return answer;
+    }
+}
