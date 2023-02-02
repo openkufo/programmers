9d07000a3c0e49833eb83ee8db5efa0de5d02a31
paul Han <openkufo@gmail.com>
2023-02-02 14:45:02 +0900

    Add Solution 120806

---
+++ src/com/paulhan/programmers/s120806/Solution.java
@@ -0,0 +1,13 @@
+package com.paulhan.programmers.s120806;
+
+public class Solution {
+    public static void main(String[] args) {
+        new Solution().solution(1, 16);
+    }
+
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        System.out.println((int)((double) num1 / num2 * 1000));
+        return answer;
+    }
+}
