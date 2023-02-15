98cc77e9b934a37081ef6e7dabc7408ca912493d
paul Han <openkufo@gmail.com>
2023-02-15 15:59:01 +0900

    Add Solution 120902

---
+++ src/com/paulhan/programmers/s120902/Solution.java
@@ -0,0 +1,29 @@
+package com.paulhan.programmers.s120902;
+
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) {
+        int answer = new Solution().solution("3 + 4 - 4");
+        System.out.println(answer);
+    }
+
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        StringTokenizer st = new StringTokenizer(my_string);
+        answer = Integer.parseInt(st.nextToken());
+
+        while(st.hasMoreTokens()){
+            boolean isPlusOperator = st.nextToken().equals("+");
+            if(isPlusOperator){
+                answer += Integer.parseInt(st.nextToken());
+            } else {
+                answer -= Integer.parseInt(st.nextToken());
+            }
+        }
+
+        return answer;
+    }
+}
