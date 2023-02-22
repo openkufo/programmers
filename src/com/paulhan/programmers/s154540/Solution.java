d6aaeb1627e97888b6c1ae2c105cca03179c5dea
paul Han <openkufo@gmail.com>
2023-02-22 16:59:39 +0900

    Add Solution 154540

---
+++ src/com/paulhan/programmers/s154540/Solution.java
@@ -0,0 +1,19 @@
+package com.paulhan.programmers.s154540;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] answer = new Solution().solution(new String[]{"X591X","X1X5X","X231X", "1XXX1"});
+    }
+
+    public int[] solution(String[] maps) {
+        int[] answer = {};
+        int[][] hasNumberIndex = new int[maps.length][maps[0].length()];
+
+        for(int i = 0; i < hasNumberIndex.length; i++){
+            for(int j = 0; j < hasNumberIndex[i].length; i++){
+                
+            }
+        }
+        return answer;
+    }
+}
