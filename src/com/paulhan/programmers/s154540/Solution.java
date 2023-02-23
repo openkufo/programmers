ac21fa79c44d33ed8ffa13795c0677843847115e
paul Han <openkufo@gmail.com>
2023-02-23 20:46:43 +0900

    무인도 여행 해결중.....

--- src/com/paulhan/programmers/s154540/Solution.java
+++ src/com/paulhan/programmers/s154540/Solution.java
@@ -1,5 +1,7 @@
 package com.paulhan.programmers.s154540;
 
+import java.util.ArrayList;
+
 public class Solution {
     public static void main(String[] args) {
         int[] answer = new Solution().solution(new String[]{"X591X","X1X5X","X231X", "1XXX1"});
@@ -7,13 +9,19 @@ public class Solution {
 
     public int[] solution(String[] maps) {
         int[] answer = {};
-        int[][] hasNumberIndex = new int[maps.length][maps[0].length()];
+        ArrayList<Integer> indexList = new ArrayList<>();
 
-        for(int i = 0; i < hasNumberIndex.length; i++){
-            for(int j = 0; j < hasNumberIndex[i].length; i++){
-                
+        for(int row = 0; row < maps.length; row++){
+            for(int col = 0; col < maps[row].length(); col++){
+                char island = maps[row].charAt(col);
+
+                if(island != 'X'){
+                    indexList.add(col);
+                }
             }
         }
+
+        System.out.println(indexList.toString());
         return answer;
     }
 }
