d349a9052c942a0c2c0dbd0131d0a2428d44f003
paul Han <openkufo@gmail.com>
2023-02-27 14:17:22 +0900

    Change Solution 120807 약수구하기 -> 숫자 비교하기

--- src/com/paulhan/programmers/s120807/Solution.java
+++ src/com/paulhan/programmers/s120807/Solution.java
@@ -1,5 +1,8 @@
 package com.paulhan.programmers.s120807;
 
 public class Solution {
-    
+    public int solution(int num1, int num2) {
+        int answer = num1 == num2 ? 1 : -1;
+        return answer;
+    }
 }
