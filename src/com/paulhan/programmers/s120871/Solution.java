c73b5565bb5f1a12d30b6a23ed80f1460bbd2422
paul Han <openkufo@gmail.com>
2023-03-24 17:24:44 +0900

    Complete Solution 120871

--- src/com/paulhan/programmers/s120871/Solution.java
+++ src/com/paulhan/programmers/s120871/Solution.java
@@ -1,63 +1,71 @@
 package com.paulhan.programmers.s120871;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public static void main(String[] args) {
-        int answer = new Solution().solution(15);
+        int answer = new Solution().solution(40);
         System.out.println("answer : " + answer);
+        System.out.println("test : " + new Solution().test(40));
     }
+
+    public int test(int n){
+        int base = 0;
+        
+        for(int i = 1; i <= n; i++){
+            base++;
+            
+            while(Integer.toString(base).contains("3") || base % 3 == 0){
+                base++;
+            }
+        }
+        return base;
+    }
+
     public int solution(int n) {
-        int answer = n;
-
-        int quotient = n / 3;
-        int threeCount = getThreeCount(n);
-        System.out.println(threeCount);
-
+        int answer = 0;
+        int threeCount = 0;
         int prevValue = 0;
-        int curValue = quotient + threeCount;
-        int compareValue = n + curValue;
+        int curValue = 0;
 
-        while(prevValue != curValue){
-            curValue = quotient + threeCount;
+        while(n >= 3){
+            threeCount = getThreeCount(n);
+            curValue = threeCount;
+            n += curValue - prevValue;
 
+            System.out.println("prevValue : " + prevValue);
+            System.out.println("curValue : " + curValue);
+            if(prevValue == curValue){
+                break;
+            }
+
+            
             prevValue = curValue;
         }
 
+        answer = n;
         return answer;
     }
 
     public int getThreeCount(int n){
-        if(n < 10){
+        if(n < 3){
             return 0;
         }
 
-        if(n == 100){
-            return 9;
-        }
-
+        int result = n / 3;
         int tens = n / 10 % 10;
         int units = n % 10;
+
+        result += units >= 3 ? tens : tens - 1;
         
-        int result = units >= 3 ? tens : tens - 1;
+        boolean isThirty = n >= 30 && n <= 39 || n >= 130 && n <= 139;
+        if(isThirty){
+            result += units - 4;    
+        } else if(n >= 40 || n >= 140){
+            result += 7;
+        }
+        
         return result;
     }
 }
-
-// 15
-
-/*
- * 15는 3으로 나눈 몫 5와 13이 포함되어 6
- * 15 + 6 = 21
- * 
- * 21 은 3으로 나눈 몫 7과 13이 포함되어 8
- * 21 + 8 - 6 = 23
- * 
- * 23은 3으로 나눈 몫 7과 13, 23이 포함되어 9
- * 23 + 9 - 8 = 24
- * 
- * 24는 3으로 나눈 몫 8과 13, 23이 포함되어 10
- * 24 + 10 - 9 = 25
- * 
- * 25는 3으로 나눈 몫 8과 13, 23이 포함되어 10
- * 25 + 10 - 10 = 25
- * 25는 3으로 나눈 몫 이 없다.
- */
