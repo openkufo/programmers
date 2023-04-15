14b7385d7f64f683f7ff60a4047a073e0f259a4b
paul Han <openkufo@gmail.com>
2023-04-15 17:04:17 +0900

    Complete Solution 120866

--- src/com/paulhan/programmers/s120866/Solution.java
+++ src/com/paulhan/programmers/s120866/Solution.java
@@ -1,5 +1,7 @@
 package com.paulhan.programmers.s120866;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
         int answer = new Solution()
@@ -10,16 +12,42 @@ public class Solution {
                     {0, 0, 1, 0, 0},
                     {0, 0, 0, 0, 0}
                 });
+
+        System.out.println("answer : " + answer);
     }
     public int solution(int[][] board) {
         int answer = 0;
-        int mine = 0;
+        int[][] pos = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1}, {0, 0}, {0, 1},
+            {1, -1}, {1, 0}, {1, 1}
+        };
+
         for(int col = 0; col < board.length; col++){
             for(int row = 0; row < board[col].length; row++){
                 int cell = board[col][row];
+                boolean isBomb = cell == 1;
+                if(isBomb){
+                    for(int i = 0; i < pos.length; i++){
+                        try{
+                            int x = col + pos[i][0];
+                            int y = row + pos[i][1];
 
+                            board[x][y] = board[x][y] != 1 ? 2 : board[x][y];
+                        } catch(ArrayIndexOutOfBoundsException e){
+                            continue;
+                        }
+                    }
+                }
             }
         }
+
+        for(int col = 0; col < board.length; col++){
+            for(int row = 0; row < board[col].length; row++){
+                answer += board[col][row] == 0 ? 1 : 0;
+            }
+        }
+
         return answer;
     }
 }
