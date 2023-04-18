07ec6ccd62c96c3f7df163bdbbb58f12c55ec755
paul Han <openkufo@gmail.com>
2023-04-18 17:02:29 +0900

    Add Solution 120862

---
+++ src/com/paulhan/programmers/s120862/Solution.java
@@ -0,0 +1,22 @@
+package com.paulhan.programmers.s120862;
+
+import java.text.Collator;
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int answer = new Solution().solution(new int[]{1,2,-3,4,-5});
+        System.out.println("answer : " + answer);
+    }
+
+    public int solution(int[] numbers) {
+        int answer = 0;
+
+        Arrays.sort(numbers);
+
+        int negative = numbers[0] * numbers[1];
+        int positive = numbers[numbers.length-1] * numbers[numbers.length-2];
+        answer = positive >= negative ? positive : negative;
+        return answer;
+    }
+}
